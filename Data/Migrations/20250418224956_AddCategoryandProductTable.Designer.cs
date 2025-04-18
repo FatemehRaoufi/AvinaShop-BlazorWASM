58270025d14d6fbb89d16bd63bf86c5bcb40347d
F R <raufi4u@gmail.com>
2025-04-19 00:56:34 +0200

    Add Migration to create Category and Product Tables

--- Data/Migrations/20250418224956_AddCategoryandProductTable.Designer.cs
+++ Data/Migrations/20250418224956_AddCategoryandProductTable.Designer.cs
@@ -4,6 +4,7 @@ using AvinaShop.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AvinaShop.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250418224956_AddCategoryandProductTable")]
+    partial class AddCategoryandProductTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
